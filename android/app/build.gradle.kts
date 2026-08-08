42e14558f85f90fa9d9c402ff90fdb82397d9184
itnasbocked <floresbetancourtsantiagojacob@gmail.com>
2026-08-07 21:51:19 -0600

    Reinicio de arquitectura base

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -32,9 +31,6 @@ android {
     }
 
     buildTypes {
-        debug{
-            isDebuggable = true
-        }
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
@@ -45,8 +41,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    add("coreLibraryDesugaring", "com.android.tools:desugar_jdk_libs:2.1.4")
 }
